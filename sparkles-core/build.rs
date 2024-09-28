7386d4062cf13db87dbb4540d3be97001fc8a48f
SkyGrel19 <skibav2002@gmail.com>
2024-09-28 21:04:11 +0300

    Update license, move ENCODER_VERSION file, update build.rs

--- sparkles-core/build.rs
+++ sparkles-core/build.rs
@@ -2,11 +2,10 @@ use std::{env, fs};
 use std::path::PathBuf;
 
 fn main() -> std::io::Result<()> {
-
     let out_dir_path = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let out_file_path = out_dir_path.join("consts.rs");
 
-    let encoder_version: u32 = fs::read_to_string("../ENCODER_VERSION")?.parse().unwrap();
+    let encoder_version: u32 = fs::read_to_string("ENCODER_VERSION")?.parse().unwrap();
     
     fs::write(&out_file_path, format!("pub const ENCODER_VERSION: u32 = {};\n", encoder_version))?;
     
