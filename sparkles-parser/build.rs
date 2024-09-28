a015536f0e80af43303ea91b0b0a83155b507832
SkyGrel19 <skibav2002@gmail.com>
2024-09-28 23:00:02 +0300

    get rid of protobuf-src

--- sparkles-parser/build.rs
+++ sparkles-parser/build.rs
@@ -11,17 +11,6 @@ fn main() -> std::io::Result<()> {
     fs::write(&out_file_path, format!("pub const ENCODER_VERSION: u32 = {};\n", encoder_version))?;
 
     // 2. Build protos
-    let protoc = prost_build::protoc_from_env();
-    if std::process::Command::new(protoc)
-        .arg("--version")
-        .output()
-        .is_err()
-    {
-        #[allow(unused_unsafe)]
-        unsafe {
-            std::env::set_var("PROTOC", protobuf_src::protoc());
-        }
-    }
     // https://github.com/google/perfetto/blob/main/protos/perfetto/trace/perfetto_trace.proto
     prost_build::compile_protos(&["perfetto_trace.proto"], &["protos"])?;
 
