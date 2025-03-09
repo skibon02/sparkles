e3880207c91bf9cae67680c75a6fefdfe646c8c7
SkyGrel19 <skibav2002@gmail.com>
2025-03-10 00:05:32 +0300

    Move PROTOCOL_VERSION to sparkles-core

--- sparkles-core/build.rs
+++ sparkles-core/build.rs
@@ -5,7 +5,7 @@ fn main() -> std::io::Result<()> {
     let out_dir_path = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let out_file_path = out_dir_path.join("consts.rs");
 
-    let proto_version = fs::read_to_string("../PROTOCOL_VERSION")?.split(".").map(|s| s.parse::<u8>().unwrap()).collect::<Vec<_>>();
+    let proto_version = fs::read_to_string("PROTOCOL_VERSION")?.split(".").map(|s| s.parse::<u8>().unwrap()).collect::<Vec<_>>();
     let proto_version = (proto_version[0], proto_version[1]);
 
     fs::write(&out_file_path, format!("pub const PROTOCOL_VERSION: (u8, u8) = {:?};\n", proto_version))?;
